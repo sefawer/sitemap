b9776ff9abd620af015ee7241fc595df5f3a59bd
sefawer <sefawer@gmail.com>
2023-08-23 23:56:34 +0300

    first commit

---
+++ home.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>
+        </title>
+    </head>
+    <body>
+<?php
+$pdo = new PDO("mysql:host=localhost;dbname=supermomo", 'root', '');
+$pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+
+$unbufferedResult = $pdo->query("SELECT * FROM links");
+foreach ($unbufferedResult as $row) {
+    echo '<a href="'.$row['url_key'].'">'.$row['title'].'</a><br />';
+}
+?>
+    </body>
+</html>
